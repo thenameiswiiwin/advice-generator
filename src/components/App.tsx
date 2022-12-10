1fc180ec668af7ea8b6c523e949c5bb541306a0e
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-10 03:12:25 -0600

    system design

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
+import Dice from '../assets/images/icon-dice.svg';
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">Hello</h1>
+    <div className="bg-secondary-200 px-6">
+      <h1>ADVICE #117</h1>
+      <p>
+        &ldquo;It is easy to sit up and take notice, what's difficult is getting
+        up and taking action.&rdquo;
+      </p>
+      <button type="button" className="rounded-full bg-primary-200 p-5">
+        <img src={Dice} alt="dice" />
+      </button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        titleSm: ['0.6875rem', '0.939rem'],
+        titleLg: ['0.8125rem', '1.11rem'],
+        bodySm: ['1.5rem', '2.05rem'],
+        bodyLg: ['1.75rem', '2.39rem'],
+      },
+      colors: {
+        primary: {
+          100: '#CEE3E9',
+          200: '#53FFAA',
+        },
+        secondary: {
+          100: '#202733',
+          200: '#313A48',
+          300: '#4F5D74',
+        },
+      },
+    },
   },
   plugins: [],
 };
