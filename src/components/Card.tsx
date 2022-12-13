e8eff19499bfcb0d88bea871ef868e225982b680
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-12 18:26:34 -0600

    fetch advice and id from API

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from 'react';
 import Dice from '../assets/images/icon-dice.svg';
 import { MobileDivider } from '../assets/index';
 
+type AdviceProps = {
+  id: number;
+  advice: string;
+};
+
 const Card = () => {
+  const [advice, setAdvice] = useState({} as AdviceProps);
+
+  const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice', {
+      method: 'GET',
+      cache: 'no-cache',
+    });
+    const data = await response.json();
+    setAdvice(data.slip);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="relative flex flex-col items-center justify-center gap-6 rounded-card bg-secondary-200 py-10 pt-6 text-center">
-      <h1>ADVICE #117</h1>
-      <p>
-        &ldquo;It is easy to sit up and take notice, what's difficult is getting
-        up and taking action.&rdquo;
-      </p>
+    <div className="relative flex min-h-[18rem] max-w-lg flex-col items-center justify-center gap-6 rounded-card bg-secondary-200 py-10 px-6 pt-6 text-center lg:px-12">
+      <h1>ADVICE #{advice.id} </h1>
+      <p>{advice.advice}</p>
       <img src={MobileDivider} alt="divider" className="pb-8" />
-      <button type="button" className="btn">
+      <button type="button" className="btn" onClick={fetchAdvice}>
         <img src={Dice} alt="dice" />
       </button>
     </div>
